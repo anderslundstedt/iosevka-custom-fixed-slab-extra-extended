febe6b401559e036cedc958cc5930b620aeb1217
Anders Lundstedt <git@anderslundstedt.se>
2026-05-28 15:56:02 +0200

    change flake inputs

--- flake.nix
+++ flake.nix
@@ -1,10 +1,13 @@
 # flake based on
 # https://github.com/NixOS/templates/blob/ad0e221dda33c4b564fad976281130ce34a20cb9/bash-hello/flake.nix
 {
-  description        = "My custom build of Iosevka";
-  inputs.nixpkgs.url = "nixpkgs/nixpkgs-unstable";
+  description                 = "My custom build of Iosevka";
 
-  outputs = {nixpkgs, ...}: (
+  inputs.nixpkgs-unstable.url = "nixpkgs/nixpkgs-unstable";
+  inputs.nixpkgs-darwin.url   = "nixpkgs/nixpkgs-25.11-darwin";
+  inputs.nixpkgs-linux.url    = "nixpkgs/nixos-25.11";
+
+  outputs = {nixpkgs-unstable,nixpkgs-darwin,nixpkgs-linux,...}: (
     let
       # confirmed to work on the following systems
       systems-linux    = ["x86_64-linux"  "aarch64-linux"];
@@ -13,10 +16,19 @@
 
       # helper function to generate an attrset
       # '{ x86_64-linux = f "x86_64-linux"; ... }'.
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      forAllSystems = nixpkgs-unstable.lib.genAttrs supportedSystems;
+
+      get-nixpkgs-stable = system: (
+        if      builtins.elem system systems-linux  then
+          nixpkgs-linux
+        else if builtins.elem system systems-darwin then
+          nixpkgs-darwin
+        else
+          nixpkgs-unstable
+      );
     in {
       defaultPackage = forAllSystems(system:
-        nixpkgs.legacyPackages.${system}.iosevka.override {
+        (get-nixpkgs-stable system).legacyPackages.${system}.iosevka.override {
           set        = "Custom-Fixed-Slab-ExtraExtended";
           privateBuildPlan = {
             family  = "Iosevka Custom Fixed Slab Extra Extended";
