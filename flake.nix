d17b0fb3b1cad19080e533df9ffbb9e0dd3ac9d2
Anders Lundstedt <git@anderslundstedt.se>
2026-08-26 17:01:24 +0200

    use flake-utils

--- flake.nix
+++ flake.nix
@@ -1,57 +1,42 @@
-# flake based on
-# https://github.com/NixOS/templates/blob/ad0e221dda33c4b564fad976281130ce34a20cb9/bash-hello/flake.nix
 {
   description = "My custom build of Iosevka";
 
-  inputs.pins.url = "github:anderslundstedt/nix-pins";
+  inputs.pins.url            = "github:anderslundstedt/nix-pins";
+  inputs.flake-utils.follows = "pins/flake-utils";
 
-  inputs.nixpkgs-unstable.follows      = "pins/nixpkgs-unstable";
-  inputs.nixpkgs-darwin-stable.follows = "pins/nixpkgs-darwin-stable";
-  inputs.nixpkgs-linux-stable.follows  = "pins/nixos-stable";
-
-  outputs = {nixpkgs-unstable,nixpkgs-darwin-stable,nixpkgs-linux-stable,...}: (
+  outputs = inputs@{...}:
     let
       # confirmed to work on the following systems
       systems-linux    = ["x86_64-linux"  "aarch64-linux"];
-      systems-darwin   = ["x86_64-darwin" "aarch64-darwin"];
+      systems-darwin   = ["aarch64-darwin"];
       supportedSystems = systems-linux ++ systems-darwin;
-
-      # helper function to generate an attrset
-      # '{ x86_64-linux = f "x86_64-linux"; ... }'.
-      forAllSystems = nixpkgs-unstable.lib.genAttrs supportedSystems;
-
-      get-nixpkgs-stable = system: (
-        if      builtins.elem system systems-linux  then
-          nixpkgs-linux-stable
-        else if builtins.elem system systems-darwin then
-          nixpkgs-darwin-stable
-        else
-          nixpkgs-unstable
-      );
-    in {
-      defaultPackage = forAllSystems(system:
-        (get-nixpkgs-stable system).legacyPackages.${system}.iosevka.override {
-          set        = "Custom-Fixed-Slab-ExtraExtended";
-          privateBuildPlan = {
-            family  = "Iosevka Custom Fixed Slab Extra Extended";
-            spacing = "fixed";
-            serifs  = "slab";
-            widths  = {
-              "" = {
-                shape = 650;
-                menu  = 7;
-                css   = "expanded";
+    in
+      inputs.flake-utils.lib.eachSystem supportedSystems (system:
+        let
+          nixpkgs-stable =
+            inputs.pins.nixpkgs-stable.${system}.legacyPackages.${system};
+        in {
+          packages.default = nixpkgs-stable.iosevka.override {
+            set        = "Custom-Fixed-Slab-ExtraExtended";
+            privateBuildPlan = {
+              family  = "Iosevka Custom Fixed Slab Extra Extended";
+              spacing = "fixed";
+              serifs  = "slab";
+              widths  = {
+                "" = {
+                  shape = 650;
+                  menu  = 7;
+                  css   = "expanded";
+                };
               };
+              variants.design = {
+                zero       = "unslashed";
+                m          = "top-left-and-bottom-right-serifed";
+                lower-chi  = "semi-chancery-straight-serifed";
+              };
+              exportGlyphNames = false;
             };
-            variants.design = {
-              zero       = "unslashed";
-              m          = "top-left-and-bottom-right-serifed";
-              lower-chi  = "semi-chancery-straight-serifed";
-            };
-            exportGlyphNames = false;
           };
         }
       );
-    }
-  );
 }
